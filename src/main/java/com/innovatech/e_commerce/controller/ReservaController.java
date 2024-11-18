2791b81f53946cb2375189f5667e8c2973e9d6c5
Sebootas <dupersebas@gmail.com>
2024-11-17 23:38:22 -0500

    pedid

--- src/main/java/com/innovatech/e_commerce/controller/ReservaController.java
+++ src/main/java/com/innovatech/e_commerce/controller/ReservaController.java
@@ -15,15 +15,15 @@ public class ReservaController {
 
     public ReservaController() {
         // Datos mockeados
-        reservas.add(new Reserva(1, "Catering", "19-11-2024", "19-11-2024", "Home", 100.0));
-        reservas.add(new Reserva(2, "Fotografía", "31-3-2024", "31-3-2024", "StudioFlash", 250.0));
-        reservas.add(new Reserva(3, "Decoración", "15-4-2024", "16-4-2024", "ArtDeco", 300.0));
-        reservas.add(new Reserva(4, "Música en vivo", "20-5-2024", "20-5-2024", "MusicWorld", 500.0));
-        reservas.add(new Reserva(5, "Animación", "05-6-2024", "06-6-2024", "FunParty", 150.0));
-        reservas.add(new Reserva(6, "Seguridad", "22-7-2024", "23-7-2024", "SafeGuard", 200.0));
-        reservas.add(new Reserva(7, "Sonido", "07-8-2024", "07-8-2024", "SoundPro", 400.0));
-        reservas.add(new Reserva(8, "Iluminación", "25-9-2024", "26-9-2024", "LightShow", 350.0));
-        reservas.add(new Reserva(9, "Transporte", "12-10-2024", "12-10-2024", "MoveFast", 180.0));
+        reservas.add(new Reserva(1L, "Catering", "19-11-2024", "19-11-2024", "Home", 100.0));
+        reservas.add(new Reserva(2L, "Fotografía", "31-3-2024", "31-3-2024", "StudioFlash", 250.0));
+        reservas.add(new Reserva(3L, "Decoración", "15-4-2024", "16-4-2024", "ArtDeco", 300.0));
+        reservas.add(new Reserva(4L, "Música en vivo", "20-5-2024", "20-5-2024", "MusicWorld", 500.0));
+        reservas.add(new Reserva(5L, "Animación", "05-6-2024", "06-6-2024", "FunParty", 150.0));
+        reservas.add(new Reserva(6L, "Seguridad", "22-7-2024", "23-7-2024", "SafeGuard", 200.0));
+        reservas.add(new Reserva(7L, "Sonido", "07-8-2024", "07-8-2024", "SoundPro", 400.0));
+        reservas.add(new Reserva(8L, "Iluminación", "25-9-2024", "26-9-2024", "LightShow", 350.0));
+        reservas.add(new Reserva(9L, "Transporte", "12-10-2024", "12-10-2024", "MoveFast", 180.0));
     }
 
     @GetMapping("/api/reservas")
